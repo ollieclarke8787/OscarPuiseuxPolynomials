403da76149d3352f203e38b6b4f49c9da149f2c4
Oliver Clarke <oliver.clarke.crgs@gmail.com>
2025-10-08 14:32:32 +0100

    testbot fix syntax of print
    Updated println syntax and reordered imports.

--- tests/testbot.jl
+++ tests/testbot.jl
@@ -1,11 +1,12 @@
 # This is testbot
 # The code in this file runs anytime code is pushed or a pull request is made to the main branch
 
-println!("test")
+println("test")
 
 using Oscar
+
+Kt, (t1,t2) = puiseux_polynomial_ring(QQ, ["t1","t2"])
+
 using Pkg
 Pkg.add(OscarPuiseuxPolynomial)
 using OscarPuiseuxPolynomial
-
-Kt, (t1,t2) = puiseux_polynomial_ring(QQ, ["t1","t2"])
