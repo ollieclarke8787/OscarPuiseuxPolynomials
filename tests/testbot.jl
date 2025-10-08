f7fbe9b6a788bce8ff27fd2f9350913a4afe6d15
Oliver Clarke <oliver.clarke.crgs@gmail.com>
2025-10-08 14:19:42 +0100

    testing testbot code

--- tests/testbot.jl
+++ tests/testbot.jl
@@ -1,6 +1,11 @@
 # This is testbot
 # The code in this file runs anytime code is pushed or a pull request is made to the main branch
 
-using Oscar, OscarPuiseuxPolynomial
+println!("test")
+
+using Oscar
+using Pkg
+Pkg.add(OscarPuiseuxPolynomial)
+using OscarPuiseuxPolynomial
 
 Kt, (t1,t2) = puiseux_polynomial_ring(QQ, ["t1","t2"])
