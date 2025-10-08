a19959773ccf0b5503faa87f8f718fecd7b6d692
Oliver Clarke <oliver.clarke.crgs@gmail.com>
2025-10-08 16:24:18 +0100

    Refactor testbot.jl by cleaning up package usage
    Removed unnecessary package instantiation and addition.

--- tests/testbot.jl
+++ tests/testbot.jl
@@ -1,12 +1,9 @@
 # This is testbot
 # The code in this file runs anytime code is pushed or a pull request is made to the main branch
 
-using Pkg
-Pkg.instantiate()
-
 using Oscar
+using OscarPuiseuxPolynomial
 
 Kt, (t1,t2) = puiseux_polynomial_ring(QQ, ["t1","t2"])
 
-Pkg.add(OscarPuiseuxPolynomial)
-using OscarPuiseuxPolynomial
+
