dab4e8ff9ed49c63153b6bf71a2fed9c16c43cbe
Yue Ren <yue.ren.kl@gmail.com>
2025-11-04 08:40:38 +0000

    Adding tropical features (#11)
    * new: TropicalSemiringMap for PuiseuxMPolyRing
    
    * new: tropical hypersurfaces
    
    * fix: julia version for CI

--- src/OscarPuiseuxPolynomial.jl
+++ src/OscarPuiseuxPolynomial.jl
@@ -8,6 +8,10 @@ include("imports.jl")
 # source files
 include("puiseux_polynomial.jl")
 
+# (non-generic) tropical features
+include("semiring_map.jl")
+include("hypersurface.jl")
+
 # functions we define
 include("exports.jl")
 
